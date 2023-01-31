f850c079d0d25d11200a67dcd1530848c6e8d440
Jared-Thompkins <jaredthompkins21@gmail.com>
2023-01-31 03:40:24 -0600

    first commit

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,16 +8,102 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var usedWords = [String]()
+    @State var rootWord = ""
+    @State var newWord = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { i in
+                        HStack {
+                            Image(systemName: "\(i.count).circle")
+                            Text(i)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
-        .padding()
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "BTFail22"
+                return
+            }
+        }
+        fatalError("Could not load start.txt.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempword = rootWord
+        
+        for letter in word {
+            if let pos = tempword.firstIndex(of: letter) {
+                tempword.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
